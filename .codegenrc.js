40d72669687e720bc9fabfcf9f36021c1ff4fe77
dazulu <definitive@outlook.com>
2021-03-23 23:32:21 +0100

    style: linting

--- .codegenrc.js
+++ .codegenrc.js
@@ -1,10 +1,10 @@
-require('dotenv').config()
+require('dotenv').config();
 
 module.exports = {
-	schema: `https://graphql.contentful.com/content/v1/spaces/${process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID}/environments/master?access_token=${process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN}`,
-	generates: {
-		"./types/contentful.ts": {
-			plugins: ["typescript", "typescript-operations"],
-		},
-	},
+  schema: `https://graphql.contentful.com/content/v1/spaces/${process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID}/environments/master?access_token=${process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN}`,
+  generates: {
+    './types/contentful.ts': {
+      plugins: ['typescript', 'typescript-operations'],
+    },
+  },
 };
