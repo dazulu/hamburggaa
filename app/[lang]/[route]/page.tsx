32d345496926259b770600fbb7930d555432d722
Adrian Payne <dazulu@users.noreply.github.com>
2024-05-11 23:03:27 +0200

    feat: dynamic route pages

--- app/[lang]/[route]/page.tsx
+++ app/[lang]/[route]/page.tsx
@@ -1,10 +1,10 @@
 import Template from '@/ui/template/page';
-import { LOCALES } from '../../i18n';
+import { LOCALES } from '../../../i18n';
 import { query as pageQuery } from '@/queries/page';
 
 import type { NavigationConfigCollection } from '@/types/contentful';
 
-async function getData(lang) {
+async function getData(lang, route) {
   const pageResponse = await fetch(
     `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master`,
     {
@@ -15,7 +15,7 @@ async function getData(lang) {
       },
       body: JSON.stringify({
         query: pageQuery,
-        variables: { dir: 'ROOT', locale: lang },
+        variables: { dir: route, locale: lang },
       }),
     }
   );
@@ -36,9 +36,11 @@ export async function generateStaticParams() {
   return LOCALES.map((lang) => ({ lang }));
 }
 
-export default async function Page({ params: { lang } }): Promise<JSX.Element> {
+export default async function Page({
+  params: { lang, route },
+}): Promise<JSX.Element> {
   // Fetch data directly in a Server Component
-  const data = await getData(lang);
+  const data = await getData(lang, route);
   // Forward fetched data to your Client Component
   return <Template data={data} />;
 }
