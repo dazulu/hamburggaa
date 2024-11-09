46abc90fd758ff80fc7fb32ac864c38e82a8b0a5
Adrian Payne <dazulu@users.noreply.github.com>
2024-11-09 20:51:11 +0100

    refactor: get locale from next-intl
    - language switcher component

--- ui/header/language-switcher/index.tsx
+++ ui/header/language-switcher/index.tsx
@@ -5,16 +5,16 @@ import Image from "next/image";
 import flagEN from "@/public/images/flag_en.png";
 import flagDE from "@/public/images/flag_de.png";
 
-import { useLang } from "@/hooks/use-lang";
 import { Link } from "@/i18n/routing";
 import { LOCALES } from "@/i18n/locales";
 
 import { Locale } from "@/types/i18n";
 
 import styles from "./styles.module.css";
+import { useLocale } from "next-intl";
 
 export const LanguageSwitcher = () => {
-  const activeLocale = useLang();
+  const activeLocale = useLocale();
 
   return (
     <div className={styles.links}>
