11a5083b671239104ad37d15ce9f55cc0c5ac0f9
frontend.irish <adrian@frontend.irish>
2025-03-21 11:41:43 +0100

    feat: improve module helper util
    - adds switch case and import
    - adds json dump on page

--- create-module.js
+++ create-module.js
@@ -1,17 +1,17 @@
 const fs = require("fs");
 const path = require("path");
 
+// Check if module name is provided
 const moduleName = process.argv[2];
-
 if (!moduleName) {
   console.error("Please provide a module name as an argument.");
   process.exit(1);
 }
 
-const modulePath = path.join("ui", "modules", moduleName);
-
-if (!fs.existsSync(modulePath)) {
-  fs.mkdirSync(modulePath);
+// Create module directory
+const moduleDirPath = path.join("ui", "modules", moduleName);
+if (!fs.existsSync(moduleDirPath)) {
+  fs.mkdirSync(moduleDirPath);
 } else {
   console.error(`Module ${moduleName} already exists.`);
   process.exit(1);
@@ -22,16 +22,19 @@ const capitalisedName = moduleName[0].toUpperCase() + moduleName.substring(1);
 const indexContent = `
 import styles from './styles.module.css';
 
-export const ${capitalisedName} = ({ module }: { module: ${capitalisedName} }) => {
+export const ${capitalisedName} = ({ module }: { module: any }) => {
   return (
     <div className={styles.container}>
       <h1>${capitalisedName}</h1>
+      <pre>
+        <code>{JSON.stringify(module, null, 2)}</code>
+      </pre>
     </div>
   );
 }
 `;
 
-fs.writeFileSync(path.join(modulePath, "index.tsx"), indexContent);
+fs.writeFileSync(path.join(moduleDirPath, "index.tsx"), indexContent);
 
 // Create styles.module.css
 const stylesContent = `
@@ -39,6 +42,35 @@ const stylesContent = `
 }
 `;
 
-fs.writeFileSync(path.join(modulePath, "styles.module.css"), stylesContent);
+fs.writeFileSync(path.join(moduleDirPath, "styles.module.css"), stylesContent);
+
+// Edit /ui/modules/index.tsx
+const modulesFilePath = path.join("ui", "modules", "index.tsx");
+
+// Check if the file exists
+if (!fs.existsSync(modulesFilePath)) {
+  console.error("File /ui/modules/index.tsx does not exist.");
+  process.exit(1);
+}
+
+try {
+  const modulesContentData = fs.readFileSync(modulesFilePath, "utf-8");
+
+  // Add a new switch case for the module
+  const newCase = `case "${capitalisedName}":\n\t\t\t\t\t\treturn <${capitalisedName} module={module} />;`;
+  const updatedModulesContentWithSwitch = modulesContentData.replace(
+    /default:/,
+    `${newCase}\n\t\t\t\t\tdefault:`
+  );
+
+  const newImport = `import { ${capitalisedName} } from './${moduleName}';\n`;
+  const updatedContentWithImport = newImport + updatedModulesContentWithSwitch;
+
+  // Write the updated content back to the file
+  fs.writeFileSync(modulesFilePath, updatedContentWithImport);
+} catch (error) {
+  console.error("Error updating /ui/modules/index.tsx:", error);
+  process.exit(1);
+}
 
 console.log(`Module ${moduleName} created successfully.`);
