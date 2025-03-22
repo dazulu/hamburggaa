d5528eb388d1a1ceae2e117797082ab652cba56e
frontend.irish <adrian@frontend.irish>
2025-03-22 22:31:21 +0100

    feat: typography adjustments

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,19 @@
 import "@/styles/global.css";
 import "@/styles/variables.css";
 
+import { bodyFont, headlineFont } from "@/services/fonts";
+
 import { CSSVariables } from "@/components/css-variables";
-import { Oswald } from "next/font/google";
 import { ThemeCollection } from "@/types/contentful";
 import { getData } from "@/services/get-data";
 import { query } from "@/queries/config";
-
-const oswald = Oswald({
-  weight: "700",
-  subsets: ["latin"],
-});
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
   const data = await getData<ThemeCollection>({ query });
   const { primaryColour, secondaryColour } = data.themeCollection.items[0];
 
   return (
-    <html lang="en" className={oswald.className}>
+    <html lang="en" className={`${bodyFont.variable} ${headlineFont.variable}`}>
       <body>
         <CSSVariables
           primaryColour={primaryColour}
---
+++ services/fonts.ts
@@ -0,0 +1,13 @@
+import { Oswald, Roboto } from "next/font/google";
+
+export const headlineFont = Oswald({
+  subsets: ["latin"],
+  weight: "700",
+  variable: "--font-headline",
+});
+
+export const bodyFont = Roboto({
+  subsets: ["latin"],
+  variable: "--font-body",
+  weight: "300",
+});
