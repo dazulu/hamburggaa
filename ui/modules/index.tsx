3b15c08065bae94a4725666755d5cadb76c94ced
dazulu <adrian@frontend.irish>
2025-03-28 20:11:38 +0100

    refactor: module mapping

--- ui/modules/index.tsx
+++ ui/modules/index.tsx
@@ -9,51 +9,34 @@ import { ModuleImages } from "./images";
 import { ModulePersonList } from "./person-list";
 import { PageModulesItem } from "@/types/contentful";
 
+const moduleMapping = {
+  Faqs: ModuleFaq,
+  Footer: ModuleFooter,
+  Header: ModuleHeader,
+  Hero: ModuleHero,
+  Images: ModuleImages,
+  ImageText: ModuleImageText,
+  PersonList: ModulePersonList,
+  Showcase: ModuleShowcase,
+  Text: ModuleText,
+};
+
 export const Modules = ({ modules }: { modules: PageModulesItem[] }) => {
-  return (
-    <>
-      {modules.map((module, index) => {
-        switch (module.__typename) {
-          case "Faqs": {
-            return <ModuleFaq key={module.sys.id} module={module} />;
-          }
-          case "Footer": {
-            return <ModuleFooter key={module.sys.id} module={module} />;
-          }
-          case "Header": {
-            return <ModuleHeader key={module.sys.id} module={module} />;
-          }
-          case "Hero": {
-            return <ModuleHero key={module.sys.id} module={module} />;
-          }
-          case "Images": {
-            return <ModuleImages key={module.sys.id} module={module} />;
-          }
-          case "ImageText": {
-            return <ModuleImageText key={module.sys.id} module={module} />;
-          }
-          case "PersonList": {
-            return <ModulePersonList key={module.sys.id} module={module} />;
-          }
-          case "Showcase": {
-            return <ModuleShowcase key={module.sys.id} module={module} />;
-          }
-          case "Text": {
-            return <ModuleText key={module.sys.id} module={module} />;
-          }
-          default: {
-            return (
-              <pre key={index}>
-                <code>
-                  NOT MAPPED
-                  <br />
-                  {JSON.stringify(module, null, 2)}
-                </code>
-              </pre>
-            );
-          }
-        }
-      })}
-    </>
-  );
+  return modules.map((module, index) => {
+    const ModuleComponent = moduleMapping[module.__typename];
+
+    if (!ModuleComponent) {
+      return (
+        <pre key={module.sys.id}>
+          <code>
+            NOT MAPPED
+            <br />
+            {JSON.stringify(module, null, 2)}
+          </code>
+        </pre>
+      );
+    }
+
+    return <ModuleComponent key={module.sys.id} module={module} />;
+  });
 };
