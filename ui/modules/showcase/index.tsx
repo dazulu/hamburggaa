42df43f55b11d2165e37223f206561b24c69c804
frontend.irish <adrian@frontend.irish>
2025-03-28 15:52:29 +0100

    feat: add cta to showcase module

--- ui/modules/showcase/index.tsx
+++ ui/modules/showcase/index.tsx
@@ -1,14 +1,18 @@
+import { ButtonLink } from "@/ui/button-link";
 import Image from "next/image";
 import { Showcase } from "@/types/contentful";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import styles from "./styles.module.css";
 
 export const ModuleShowcase = ({ module }: { module: Showcase }) => {
-  const { headline, image, text } = module;
+  const { callToActionLink, headline, image, text } = module;
   return (
     <div className={styles.container}>
       {headline && <p>{headline}</p>}
-      <div>{text && documentToReactComponents(text.json)}</div>
+      <div>
+        {text && documentToReactComponents(text.json)}{" "}
+        {callToActionLink && <ButtonLink {...callToActionLink} />}
+      </div>
       {image && (
         <Image
           alt={image.description || ""}
