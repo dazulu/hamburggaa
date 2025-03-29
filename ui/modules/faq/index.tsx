c8ee5a4d3af4ca1bec0fcc0170ae533dc342999b
dazulu <adrian@frontend.irish>
2025-03-29 13:02:42 +0100

    feat: style faq question pairs

--- ui/modules/faq/index.tsx
+++ ui/modules/faq/index.tsx
@@ -2,21 +2,28 @@ import { Faqs } from "@/types/contentful";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import React, { Fragment } from "react";
 
+import styles from "./styles.module.css";
+
 export const ModuleFaq = ({ module }: { module: Faqs }) => {
-  const { title, description, questions } = module;
+  const { sys, title, description, questions } = module;
 
   return (
-    <div>
+    <div className={styles.container}>
       <h2>{title}</h2>
       {documentToReactComponents(description.json)}
-      <dl>
-        {questions.map(({ id, key: question, value: answer }) => (
-          <Fragment key={id}>
-            <dt>{question}</dt>
-            <dd>{answer}</dd>
-          </Fragment>
+      <div className={styles.questions}>
+        {questions.map(({ id, key: question, value: answer }, index) => (
+          <details
+            key={id}
+            name={`faq_${sys.id}`}
+            open={index === 0}
+            className={styles.details}
+          >
+            <summary className={styles.summary}>{question}</summary>
+            {answer}
+          </details>
         ))}
-      </dl>
+      </div>
     </div>
   );
 };
