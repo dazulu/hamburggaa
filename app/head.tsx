ebae4cb5326b076328d9ad90f5e24b05b6d79691
frontend.irish <adrian@frontend.irish>
2025-06-21 22:11:59 +0200

    feat: title

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,10 @@
 export default function Head() {
   return (
     <>
-      <title>My Next.js App</title>
+      <title>
+        Hamburg GAA | Hurling, Camogie and Gaelic Football club in Hamburg,
+        Germany
+      </title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="build-time" content={new Date().toString()} />
