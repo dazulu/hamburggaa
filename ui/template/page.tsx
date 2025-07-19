018d9886bdfc93ff03f8df9fadc3d9b9592cdb4c
apayne <adrian@frontend.irish>
2025-07-19 18:29:14 +0200

    feat: parent style for container query

--- ui/template/page.tsx
+++ ui/template/page.tsx
@@ -1,9 +1,11 @@
 import type { PageData } from "@/types/page";
 import { Modules } from "@/ui/modules";
 
+import styles from "./styles.module.css";
+
 export default function Page({ data }: PageData) {
 	return (
-		<div>
+		<div className={styles.container}>
 			<Modules modules={data.modulesCollection.items.filter(Boolean)} />
 		</div>
 	);
