676b0e84a656130ca84c41198ee868cfb97e7e59
frontend.irish <adrian@frontend.irish>
2025-07-24 23:19:05 +0200

    feat: lang switch a11y

--- ui/modules/header/language-switcher/index.tsx
+++ ui/modules/header/language-switcher/index.tsx
@@ -14,17 +14,25 @@ import styles from "./styles.module.css";
 export const LanguageSwitcher = () => {
 	const activeLocale = useLocale();
 
+	const getLanguageLabel = (locale: string) => {
+		return locale === Locale.EN ? "Switch to English" : "Auf Deutsch wechseln";
+	};
+
 	return LOCALES.filter((locale) => locale !== activeLocale).map((locale) => {
 		const image = locale === Locale.EN ? flagEN : flagDE;
+		const languageLabel = getLanguageLabel(locale);
+
 		return (
 			<Link
 				key={locale}
 				href="/"
 				className={styles.link}
-				title={locale.toUpperCase()}
+				aria-label={languageLabel}
+				title={languageLabel}
 				locale={locale}
 			>
 				<Image
+					aria-hidden="true"
 					alt=""
 					src={image}
 					width={38}
