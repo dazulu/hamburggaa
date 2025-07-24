43d3547e0147e3cc901ff2bba811fa311ec9af72
frontend.irish <adrian@frontend.irish>
2025-07-24 23:09:33 +0200

    feat: text module styling

--- ui/modules/text/index.tsx
+++ ui/modules/text/index.tsx
@@ -1,18 +1,19 @@
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 import type { Text } from "@/types/contentful";
+import { createRichTextRenderOptions } from "@/utils/rich-text-renderer";
 
 import styles from "./styles.module.css";
 
 export const ModuleText = ({ module }: { module: Text }) => {
 	const { text } = module;
+	const centeredClassName = module.centerText ? styles.centered : "";
+
+	const renderOptions = createRichTextRenderOptions(text?.links);
 
 	return (
-		<div className={styles.container}>
-			{text && documentToReactComponents(text.json)}
-			<pre>
-				<code>{JSON.stringify(module, null, 2)}</code>
-			</pre>
+		<div className={`${styles.container} ${centeredClassName}`}>
+			{text && documentToReactComponents(text.json, renderOptions)}
 		</div>
 	);
 };
