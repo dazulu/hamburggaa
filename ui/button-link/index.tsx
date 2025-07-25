a00cac02bcac0c281385523dc33cfee68d145265
frontend.irish <adrian@frontend.irish>
2025-07-25 14:25:22 +0200

    feat: button link styles

--- ui/button-link/index.tsx
+++ ui/button-link/index.tsx
@@ -6,15 +6,26 @@ import { getInternalLinkSlug } from "@/utils/navigation";
 
 import styles from "./styles.module.css";
 
-export const ButtonLink = async (item: ImageTextCallToActionLink) => {
+interface ButtonLinkProps {
+	variant?: "filled" | "outlined";
+	className?: string;
+}
+
+export const ButtonLink = async ({
+	variant = "filled",
+	className = "",
+	...item
+}: ButtonLinkProps & ImageTextCallToActionLink) => {
+	const buttonClasses = `${styles.button} ${styles[variant]} ${className}`;
+
 	// Internal Nextjs routing links
-	if (item.__typename === "Page") {
+	if (item?.__typename === "Page") {
 		const href = getInternalLinkSlug(item);
 
 		return (
 			<Link
 				href={href}
-				className={styles.button}
+				className={buttonClasses}
 			>
 				{item.menuLabel}
 			</Link>
@@ -25,15 +36,16 @@ export const ButtonLink = async (item: ImageTextCallToActionLink) => {
 	return (
 		<a
 			href={item.url}
-			className={styles.button}
+			className={buttonClasses}
 			target="_blank"
 			rel="noopener noreferrer"
 		>
 			{item.menuLabel}
 			<Image
+				unoptimized
 				src="/icons/external.svg"
-				width="16"
-				height="16"
+				width="18"
+				height="18"
 				alt=""
 			/>
 		</a>
