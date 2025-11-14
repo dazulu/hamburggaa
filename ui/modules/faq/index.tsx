fb66a8dd7740efea7b22aa70e34138cff2edfb9b
frontend.irish <adrian@frontend.irish>
2025-11-14 21:25:34 +0100

    feat: faq styling

--- ui/modules/faq/index.tsx
+++ ui/modules/faq/index.tsx
@@ -11,21 +11,46 @@ export const ModuleFaq = ({ module }: { module: Faqs }) => {
 	const renderOptions = createRichTextRenderOptions(description?.links);
 
 	return (
-		<div className={`${styles.container} global-contain-width global-module-spacing`}>
-			<h2>{title}</h2>
-			{documentToReactComponents(description.json, renderOptions)}
-			<div className={styles.questions}>
-				{questions.map(({ id, key: question, value: answer }, index) => (
-					<details
-						key={id}
-						name={`faq_${sys.id}`}
-						open={index === 0}
-						className={styles.details}
-					>
-						<summary className={styles.summary}>{question}</summary>
-						{answer}
-					</details>
-				))}
+		<div className={`${styles.container} global-full-width`}>
+			<div>
+				<h2 className={styles.title}>{title}</h2>
+				{documentToReactComponents(description.json, renderOptions)}
+				<div className={styles.questions}>
+					{questions.map(({ id, key: question, value: answer }, index) => (
+						<details
+							key={id}
+							name={`faq_${sys.id}`}
+							open={index === 0}
+							className={styles.details}
+						>
+							<summary className={styles.summary}>
+								{question}
+								<svg
+									role="presentation"
+									width="24"
+									height="24"
+									viewBox="0 0 24 24"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<circle
+										cx="12"
+										cy="12"
+										r="10"
+										fill="currentColor"
+									/>
+									<path
+										d="M12 7V17M7 12H17"
+										stroke="white"
+										stroke-width="2"
+										stroke-linecap="round"
+									/>
+								</svg>
+							</summary>
+							{answer}
+						</details>
+					))}
+				</div>
 			</div>
 		</div>
 	);
